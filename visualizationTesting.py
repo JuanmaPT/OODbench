f4b6e2f5ff06656e78373ac00fc656ada1356150
Juan Manuel Peña Trapero <juanmaptcg@gmail.com>
2023-12-12 14:51:11 +0100

    Added function to show just scores in high res and 3D plot

--- visualizationTesting.py
+++ visualizationTesting.py
@@ -20,7 +20,7 @@ class_selection=  [stingray, junco, bullfrog, agama, robin, jay, bad_eagle]
 config = Configuration(model= "ResNet18", 
                        N = 1,
                        id_classes= class_selection,
-                       resolution= 4,
+                       resolution= 20,
                        dataset = "ImageNetVal_small",
                        )
 
@@ -28,7 +28,8 @@ filenamesCombis =  getCombiFromDBoptimalGoogleDrive(config)
 planesets = []
 
 for i, pathImgs in tqdm(enumerate(filenamesCombis), total=len(filenamesCombis), desc="Processing"):
-    planesets.append(Planeset(Triplet(pathImgs, config), config))
+    if i == 4:
+        planesets.append(Planeset(Triplet(pathImgs, config), config))
 
 
 
@@ -37,12 +38,16 @@ descriptors = [PlanesetInfoExtractor(planeset,config) for planeset in planesets]
 #%% 
 class_to_color = get_diff_color(planesets)
 
+create_folder(f"results/{config.modelType}_res{config.resolution}")
+
 #%% 
 for i, planeset in enumerate(planesets):
-    save_title = f"results/{config.modelType}/planeset_{i}.png"
-    print(save_title)
-    planeset.show(class_to_color,save_title)
-    break
+    i = 4
+    save_planeset_path = f"results/{config.modelType}_res{config.resolution}/planeset_{i}.png"
+    print(save_planeset_path)
+    #planeset.show_scores(class_to_color,save_planeset_path)
+    planeset.show_scores_3D(class_to_color)
+    
 
 """margin_dict = {label: [] for label in config.labels}
 for i,planeset in enumerate(planesets):
@@ -55,7 +60,6 @@ for classLabel, values in margin_dict.items():
     #if values:  # Check if the list is not empty
     plot_pmf(values, classLabel, 15, config, 0, 20)"""
 
- 
-    
-#%% Visualization Example
 
+
+# %%
